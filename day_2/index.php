dbce4218efdd8d9d92a774cd3758d1dd3f691db8
rjezra <jahoely@gmail.com>
2023-09-18 11:30:42 +0300

    first commit

---
+++ day_2/index.php
@@ -0,0 +1,57 @@
+<?php
+    /**
+    $ville = "Antananarivo";
+    $adressePost = 101;
+    $telephone = 344479594;
+    $isCapital = true;
+
+    echo $ville  ." ". $adressePost ." ". $telephone . " ". $isCapital ;
+    */
+    /** 
+    $note = 10;
+    if($note == 10){
+        echo "Tsara";
+        echo "<br>";
+    }
+    if($note != 9){
+        echo "Tsata ihany";
+    }
+    echo "<br>";
+
+    */
+
+    /**
+      $age = 18;
+    if($age >= 18){
+        echo "lehibe";
+    }elseif($age == 18){
+        echo "Tanora";
+    }elseif($age <= 18){
+        echo "Ankizy";
+    }elseif($age != 18){
+        echo "olona";
+    }else{
+        echo "mxaty";
+    }
+
+    */
+    /** 
+    echo "condition ternaire :<br>";
+    echo 18 == 18 ? "oui" : "non";
+    */
+
+    //table boolean aplique anaty if else alef git hub
+   // <=> 0, 1, -1
+
+   $heure = 18;
+   if(($heure >= 8 && $heure <= 12)||($heure >= 14 && $heure <= 16)){
+        echo "Le mangasin est ouvert";
+   }else
+   {
+        echo "Le mangasin est fermer";
+   }
+
+
+    
+
+    

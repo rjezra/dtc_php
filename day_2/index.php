db7902cdff6537e48c0f6c01f20fc0fa2b2ea8b9
rjezra <jahoely@gmail.com>
2023-09-19 09:05:16 +0300

    ajout_exo_combiné

--- day_2/index.php
+++ day_2/index.php
@@ -41,8 +41,9 @@
     */
 
     //table boolean aplique anaty if else alef git hub
-   // <=> 0, 1, -1
+   // <=> 0, 1, -1 (combine)
 
+   /**  exo1
    $heure = 18;
    if(($heure >= 8 && $heure <= 12)||($heure >= 14 && $heure <= 16)){
         echo "Le mangasin est ouvert";
@@ -50,7 +51,26 @@
    {
         echo "Le mangasin est fermer";
    }
+*/
+/**exo combine en entiers */
+$a = 2;
+$b = 5;
 
+echo $a <=> $b;
+echo "<br>";
+echo $b <=> $a;
+echo "<br>";
+echo $b <=> $b ;
+echo "<br>";
+
+/**exo combine en tableau <=> */
+$a = [1, 3, 5];
+$b = [2, 4, 5];
+echo $a <=> $b;
+echo "<br>";
+echo $b <=> $a;
+echo "<br>";
+echo $a <=> $a;
 
     
 

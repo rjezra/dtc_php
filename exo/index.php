ea3e69fcdec863c99149220c7684a3c89786bfe8
rjezra <jahoely@gmail.com>
2023-09-23 19:41:26 +0300

    exo correction

--- exo/index.php
+++ exo/index.php
@@ -1,16 +1,25 @@
 <?php
-$nom = $_POST["nom"];
-$prenom = $_POST["prenom"];
-$adresse = $_POST["adresse"];
-$enregistre = $_POST["enregistre"];
+@$nom = $_POST["nom"];
+@$prenom = $_POST["prenom"];
+@$adresse = $_POST["adresse"];
+@$enregistre = $_POST["enregistre"];
 ?>
 <form action="./index.php" method="POST">
-    <input type="text" name="nom" placeholder="Entre votrr nom">
-    <input type="text" name="prenom" placeholder="Entre votrr prenom">
-    <input type="text" name="adresse" placeholder="Entre votrr adresse">
+    <input type="text" name="nom" placeholder="Entre votre nom">
+    <input type="text" name="prenom" placeholder="Entre votre prenom">
+    <input type="text" name="adresse" placeholder="Entre votre adresse">
     <button type="submit" name = "enregistre">Enregistre</button>
 </form>
 <h1>Informations</h1>
-<p>Nom : <?=$nom?></p>
-<p>Prenom : <?=$prenom?></p>
-<p>Adresse : <?=$adresse?></p>
+<?php
+if(isset($nom)){
+    echo "<p>Nom :" .$nom. "</p>";
+}
+if(isset($prenom)){
+    echo "<p>Prenom :" .$prenom. "</p>";
+}
+
+if(isset($adresse)){
+    echo "<p>Adresse :" .$adresse. "</p>";
+}
+ ?>

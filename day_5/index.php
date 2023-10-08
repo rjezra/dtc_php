17ea608616367b84348e1fdefb2b1f348aaaa2bd
rjezra <jahoely@gmail.com>
2023-10-08 11:08:36 +0300

    ajout formulaire

--- day_5/index.php
+++ day_5/index.php
@@ -1,30 +1,28 @@
 <?php
-
-//mb_strtolower mamadika sotra ho lasa muniscul
-//mb_strtoupper mamadika soratra majuscule
-/*
-function getFullName(string $nom, ?string $prenom){ //ny ?alohan'ny type de donne miteny w nullable ilay variable
-    if(!$prenom){
-        return strtoupper($nom);
-    }
-    return  "Votre nom complet est: $nom  $prenom";
-}
-echo getFullName("Rakoto", null);
-*/
 declare(strict_types=1);
-function getFullName(string $nom, ?string $prenom = "Ravelo"): string
-{  
-    if($nom != ""){
-        preg_match("/([^A-Za-zéèàïäüùç\s])/", $nom, $result);
-        if(!empty($result)){
-            return "votre nom est icorrect";
-        }elseif(!$prenom){
-                return strtoupper($nom);
+if(isset($_POST["nom"]) && isset($_POST["prenom"])){
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    strtoupper($nom);
+    function getFullName(string $nom, ?string $prenom = "Ravelo"): string
+    {  
+        if(!empty($nom)){
+            preg_match("/([^A-Za-zéèàïäüùç\s])/", $nom, $result);
+            if(!empty($result)){
+                return "votre nom est icorrect";
+            }elseif(!$prenom){
+                    return strtoupper($nom);
+                }
+                return  "Votre nom complet est: $nom  $prenom";
+            }else{
+                return "Votre nom est vide";
             }
-            return  "Votre nom complet est: $nom  $prenom";
-        }else{
-            return "votre nom est icorrect";
-        }
-    }
-       
-echo getFullName("Doda rabe", "velo");
+    } 
+    echo getFullName($_POST["nom"], $_POST["prenom"]);
+}
+?>
+<form action="./index.php" method="POST">
+    <input type="text" name="nom"  placeholder="Entre votre nom" >
+    <input type="text" name="prenom"  placeholder="Entre votre prenom">
+    <button type="submit" name="enregistre">enregistre</button>
+</form>
